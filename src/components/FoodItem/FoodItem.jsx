04994235ff8a84754a0b4fed7d40bfae87072cc0
unknown <Bgoodluck>
2024-07-30 09:41:16 +0100

    alert

--- src/components/FoodItem/FoodItem.jsx
+++ src/components/FoodItem/FoodItem.jsx
@@ -2,6 +2,8 @@ import React, { useContext } from 'react'
 import './FoodItem.css'
 import { assets } from '../../assets/assets'
 import { StoreContext } from '../../context/StoreContext'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 function FoodItem({id, name, price, description, image, restaurant}) {
@@ -9,19 +11,32 @@ function FoodItem({id, name, price, description, image, restaurant}) {
     // const [itemCount, setItemCount] = useState(0)
     const {cartItems, addToCart, removeCartItem, url} = useContext(StoreContext)
 
+
+    const handleAddToCart = (id) => {
+      addToCart(id);
+      toast.success(`${name} added to cart!`);
+    };
+  
+    const handleRemoveCartItem = (id) => {
+      removeCartItem(id);
+      toast.error(`${name} removed from cart!`);
+    };
+
+
+
   return (
     <div className='food-item'>
       <div className="food-item-img-container">
         <img className='food-item-image' src={url + "/images/" + image} alt="" />
-        {! cartItems[id]
-         ? <img className='add' onClick={()=> addToCart(id)} src={assets.add_icon_white} alt="" />
-         : <div className='food-item-counter'>
-                <img onClick={()=> removeCartItem(id)}  src={assets.remove_icon_red} alt="" />
-                <p>{cartItems[id]}</p>
-                <img onClick={()=> addToCart(id)} src={assets.add_icon_green} alt="" />
-            </div>
-
-        }
+        {!cartItems[id] ? (
+          <img className='add' onClick={() => handleAddToCart(id)} src={assets.add_icon_white} alt="" />
+        ) : (
+          <div className='food-item-counter'>
+            <img onClick={() => handleRemoveCartItem(id)} src={assets.remove_icon_red} alt="" />
+            <p>{cartItems[id]}</p>
+            <img onClick={() => handleAddToCart(id)} src={assets.add_icon_green} alt="" />
+          </div>
+        )}
       </div>
       <div className="food-item-info">
         <div className="food-item-name-rating">
@@ -32,8 +47,9 @@ function FoodItem({id, name, price, description, image, restaurant}) {
         <p className="food-item-restaurant">{restaurant}</p>
         <p className="food-item-price">₦{price}</p>
       </div>
+      <ToastContainer />
     </div>
-  )
+  );
 }
 
 export default FoodItem
